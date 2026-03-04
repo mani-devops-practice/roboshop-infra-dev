171d87a1fea0ff8fd13b8627cd9e5edd3ae60470
manik35 <manikanta@aerospike.com>
2026-03-04 07:09:38 +0530

    creating infra using terrform for roboshop

---
+++ 00-vpc/main.tf
@@ -0,0 +1,8 @@
+module "vpc" {
+    # source = "git::https://github.com/mani-devops-practice/terraform-aws-vpc.git?ref=main"
+    source = "../../terraform-aws-vpc"
+    project = var.project
+    environment = var.environment
+    is_peering_required = true
+  
+}
